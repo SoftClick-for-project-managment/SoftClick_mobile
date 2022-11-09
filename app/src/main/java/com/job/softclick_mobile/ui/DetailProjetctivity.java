ce09ca202ff1664d1ef611b1a9050d025822fab6
youssef <zahiYoussef>
2022-11-10 00:15:26 +0100

    design detail projet

---
+++ app/src/main/java/com/job/softclick_mobile/ui/DetailProjetctivity.java
@@ -0,0 +1,16 @@
+package com.job.softclick_mobile.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.job.softclick_mobile.R;
+
+public class DetailProjetctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_projetctivity);
+    }
+}
