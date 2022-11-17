28e57b5aac22f2a749f89bd639aa751244dc9184
hindglioula123@gmail.com <hindglioula123@gmail.com>
2022-11-17 01:57:03 +0100

    add details menu

--- app/src/main/java/com/job/softclick_mobile/ui/InvoiceDetailsFragment.java
+++ app/src/main/java/com/job/softclick_mobile/ui/InvoiceDetailsFragment.java
@@ -2,11 +2,17 @@ package com.job.softclick_mobile.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.job.softclick_mobile.R;
@@ -25,6 +31,7 @@ public class InvoiceDetailsFragment extends Fragment {
     private TextView date;
     private TextView total;
     private ImageView back;
+    private ImageView more;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -60,13 +67,50 @@ public class InvoiceDetailsFragment extends Fragment {
         }
     }
 
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fContentFooter, new Fragment()).commit();
-
         View view = inflater.inflate(R.layout.fragment_invoice_details, container, false);
+        //----------------------------------------------------------------------------------------------------//
+        more=view.findViewById(R.id.moreOptions);
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(getActivity(), more);
+                popupMenu.getMenuInflater().inflate(R.menu.details_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.edit:
+                                Bundle bundle=new Bundle();
+                                bundle.putString("date",date.getText().toString());
+                                bundle.putString("total",total.getText().toString());
+                                InvoiceFormFragment invoiceFormFragment=new InvoiceFormFragment();
+                                invoiceFormFragment.setArguments(bundle);
+                                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.flContent, invoiceFormFragment).commit();
+                                break;
+
+                            case R.id.delete:
+                                Toast.makeText(getActivity(),"Under Construction ",Toast.LENGTH_LONG).show();
+                                break;
+
+                            default:
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
         date = view.findViewById(R.id.date);
         total = view.findViewById(R.id.total);
         Bundle bundle = this.getArguments();
