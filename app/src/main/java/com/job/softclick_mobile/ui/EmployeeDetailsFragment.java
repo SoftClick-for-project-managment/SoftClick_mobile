559278fdd3f4fdee1462bb2b1c6637caf2f66898
Hajar <hajarfz99@gmail.com>
2022-11-18 00:01:26 +0100

    Add Edit And Delete Employee

--- app/src/main/java/com/job/softclick_mobile/ui/EmployeeDetailsFragment.java
+++ app/src/main/java/com/job/softclick_mobile/ui/EmployeeDetailsFragment.java
@@ -1,12 +1,20 @@
 package com.job.softclick_mobile.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.job.softclick_mobile.R;
 import com.job.softclick_mobile.databinding.FragmentEmployeeDetailsBinding;
@@ -49,8 +57,91 @@ public class EmployeeDetailsFragment extends Fragment {
         binding.employeePhone.setText(employee.getEmployeePhone());
         binding.employeeFunction.setText(employee.getEmployeeFunction());
 
-        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fContentFooter,new Fragment()).commit() ;
+        if (binding.moreOptions != null) {
+            binding.moreOptions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    PopupMenu popupMenu = new PopupMenu(getActivity(), binding.moreOptions);
+                    popupMenu.getMenuInflater().inflate(R.menu.details_menu, popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            // Toast message on menu item clicked
+                            Toast.makeText(getActivity(), "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                            switch (menuItem.getItemId()) {
+                                case R.id.edit:
+
+                                    Fragment fragment = new EmployeeFormFragment();
+
+                                    Bundle bundle = new Bundle();
+                                    bundle.putSerializable("employee", employee);
+                                    fragment.setArguments(bundle);
+
+                                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                                    fragmentTransaction.replace(R.id.flContent, fragment);
+                                    fragmentTransaction.addToBackStack(null);
+                                    fragmentTransaction.commit();
+                                    break;
+
+                                case R.id.delete:
+                                    Toast.makeText(getActivity(), "Under Construction ", Toast.LENGTH_LONG).show();
+                                    AlertDialog diaBox = AskOption();
+                                    diaBox.show();
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                            return true;
+                        }
+                    });
+                    popupMenu.show();
+                }
+            });
+        }
+
+        if (binding.back != null) {
+            binding.back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    EmployeeListFragment invoiceListFragment = new EmployeeListFragment();
+                    FooterFragment footerFragment = new FooterFragment();
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fContentFooter, footerFragment).commit();
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.flContent, invoiceListFragment).commit();
+                }
+            });
+        }
+
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fContentFooter, new Fragment()).commit();
 
         return view;
+    }
+
+    private AlertDialog AskOption() {
+        AlertDialog myQuittingDialogBox = new AlertDialog.Builder(getContext())
+                // set message, title, and icon
+                .setTitle("Delete")
+                .setMessage("Do you want to Delete this Employee?")
+                .setIcon(R.drawable.delete)
+
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //your deleting code
+                        dialog.dismiss();
+                    }
+
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+
+                    }
+                })
+                .create();
+
+        return myQuittingDialogBox;
     }
 }
