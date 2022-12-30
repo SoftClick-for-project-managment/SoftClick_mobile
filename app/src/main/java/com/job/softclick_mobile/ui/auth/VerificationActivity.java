559e42b440b76a0b3c5a60c767afa8190c1775d0
wafaeabo <wafae.abouzbiba@uit.ac.ma>
2022-12-30 13:11:25 +0000

    adding login functionality

--- app/src/main/java/com/job/softclick_mobile/ui/auth/VerificationActivity.java
+++ app/src/main/java/com/job/softclick_mobile/ui/auth/VerificationActivity.java
@@ -1,25 +1,46 @@
 package com.job.softclick_mobile.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.job.softclick_mobile.databinding.ActivityVerificationBinding;
+import com.job.softclick_mobile.utils.LiveResponse;
+import com.job.softclick_mobile.viewmodels.user.IUserViewModel;
+import com.job.softclick_mobile.viewmodels.user.UserViewModel;
 
 public class VerificationActivity extends AppCompatActivity {
     private ActivityVerificationBinding binding;
+    private IUserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityVerificationBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         setContentView(view);
     }
 
     public void verify(View view){
+        /*binding.progressBar.setVisibility(View.INVISIBLE);
+
+        LiveResponse<Boolean, Throwable> userVerifyLiveResponse = userViewModel.getAll();
+
+
+        userVerifyLiveResponse.geteMutableLiveData().observe(this, new Observer() {
+            @Override
+            public void onChanged(Object o) {
+                binding.progressBar.setVisibility(View.INVISIBLE);
+                Throwable error = (Throwable) o;
+                Log.d("ERR", error.getMessage());
+            }
+        });*/
 
     }
 }
